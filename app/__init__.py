386e2dc79e834e4829be873ab1a00eea4b254a1f
fatimazhra <fatimachakour919@gmail.com>
2026-03-02 15:36:02 +0100

    tout les fichier est stockée

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+#fatimazhra #2024-06-17
+from flask import Flask
---
+++ app/user_manager.py
@@ -0,0 +1,21 @@
+class UserManager:
+    def __init__(self):
+        self.users = []
+
+    def add_user(self, username):
+        if not username :
+            raise ValueError("le nom d'utilisateur est  obligatoire")
+        if username in self.users:
+            raise ValueError("le nom d'utilisateur existe déjà")
+        self.users.append(username)
+
+    def remove_user(self, username):
+        if username not in self.users:
+            raise ValueError("le nom d'utilisateur n'existe pas")
+        self.users.remove(username)
+
+        def count_users(self):
+            return len(self.users)
+    def count_total_users(self):
+        temp =0
+        return len(users)
